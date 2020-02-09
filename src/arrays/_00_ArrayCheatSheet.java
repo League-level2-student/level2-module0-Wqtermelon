1a67764da14fb6325bb7358e0e48c227fca31c73
Andrew J Zhang <andrewjiazhangwestview@gmail.com>
2020-02-09 14:44:53 -0800

    444ghghjjytj

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,27 +1,54 @@
 package arrays;
 
+import java.util.ArrayList;
+
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
-		//1. make an array of 5 Strings
-		
-		//2. print the third element in the array
-		
-		//3. set the third element to a different value
-		
-		//4. print the third element again
-		
-		//5. use a for loop to print all the values in the array
-		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
-		
-		//6. make an array of 50 integers
-		
-		//7. use a for loop to make every value of the integer array a random number
-		
-		//8. without printing the entire array, print only the smallest number in the array
-		
-		//9 print the entire array to see if step 8 was correct
-		
-		//10. print the largest number in the array.
+		// 1. make an array of 5 Strings
+		ArrayList<String> array = new ArrayList<String>(5);
+		array.add("a");
+		array.add("ah");
+		array.add("ahh");
+		array.add("ahhh");
+		array.add("ahhhh");
+		// 2. print the third element in the array
+		System.out.println(array.get(2));
+		// 3. add the third element to a different value
+		array.add(2, "different value");
+		// 4. print the third element again
+		System.out.println(array.get(2));
+		// 5. use a for loop to print all the values in the array
+		// BE SURE TO USE THE ARRAY'S length VARIABLE
+		for (int i = 0; i < array.size(); i++) {
+			System.out.println(array.get(i));
+		}
+
+		// 6. make an array of 50 integers
+		ArrayList<Integer> array2 = new ArrayList<Integer>();
+		// 7. use a for loop to make every value of the integer array a random number
+		for (int k = 0; k < 50; k++) {
+			array2.add((int) (Math.random() * 100));
+		}
+		// 8. without printing the entire array, print only the smallest number in the
+		// array
+		int min = array2.get(0);
+		for (int j = 0; j < array2.size(); j++) {
+			if (array2.get(j) < min) {
+				min = array2.get(j);
+			}
+		}
+		System.out.println("Min" + min);
+		// 9 print the entire array to see if step 8 was correct
+		for (int m = 0; m < array2.size(); m++) {
+			System.out.println(array2.get(m));
+		}
+		// 10. print the largest number in the array.
+		int max = array2.get(0);
+		for (int j = 0; j < array2.size(); j++) {
+			if (array2.get(j) > max) {
+				max = array2.get(j);
+			}
+		}
+		System.out.println("Max" + max);
 	}
 }
--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,22 +1,38 @@
 package arrays;
 
+import java.util.ArrayList;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
-	//1. make a main method
+	// 1. make a main method
+	public static void main(String[] args) {
 
-	//2. create an array of 5 robots.
+		// 2. create an array of 5 robots.
+		ArrayList<Robot> robot = new ArrayList<Robot>();
+		// 3. use a for loop to initialize the robots.
+		for (int i = 0; i < 5; i++) {
+			robot.add(new Robot());
+			robot.get(i).setX(80 + i * 180);
+			robot.get(i).setY(500);
+		}
+		// 4. make each robot start at the bottom of the screen, side by side, facing up
+		// 5. use another for loop to iterate through the array and make each robot move
+		// a random amount less than 50.
+		for (int i = 0; i < 5; i++) {
+			while (robot.get(i).getY()!=0) {
+				robot.get(i).setY(robot.get(i).getY() - (int) (Math.random() * 50));
+			}
+		}
+		// 6. use a while loop to repeat step 5 until a robot has reached the top of the
+		// screen.
 
-	//3. use a for loop to initialize the robots.
+		// 7. declare that robot the winner and throw it a party!
 
-		//4. make each robot start at the bottom of the screen, side by side, facing up
+		// 8. try different races with different amounts of robots.
 
-	//5. use another for loop to iterate through the array and make each robot move 
-	//   a random amount less than 50.
+		// 9. make the robots race around a circular track.
 
-	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
-
-	//7. declare that robot the winner and throw it a party!
-
-	//8. try different races with different amounts of robots.
-
-	//9. make the robots race around a circular track.
+	}
 }
+	
